111f296f618aca5ffbe937ab240230ca2c5f9443
Sabino Velasquez <sabinovelasquez@gmail.com>
2016-06-14 21:15:12 -0400

    modal changes

--- src/calendar/app/core/main/day/day.js
+++ src/calendar/app/core/main/day/day.js
@@ -18,6 +18,45 @@ export default ngModule => {
         this.isToday = () => {
           return moment(`${this.DateService.activeYear}-${this.DateService.activeMonthNum}-${this.day}`).isSame(`${this.DateService.currentYear}-${this.DateService.currentMonthNum}-${this.DateService.todayNum}`);
         };
+        this.openDay = (day, month, year) => {
+          const __ = require('underscore');
+          $uibModal.open({
+            animation: true,
+            template: require('../user-modal/user-modal.jade'),
+            size: 'lg',
+            controllerAs: 'modal',
+            controller: function ModalCtrl($uibModalInstance) {
+              this.day = day;
+              this.month = month;
+              this.year = year;
+              this.checkWeather = () => {
+                const today = moment().format('YYYY-MM-DD');
+                const selected = moment([this.year, this.month, this.day]).format('YYYY-MM-DD');
+                const inrange = moment(selected).isBetween(today, moment(today).add(7, 'days'), 'days', '[]');
+                const todayw = __.find( BookAPIService.forecast, function funddate(findedday) {
+                  if (findedday.date === selected) {
+                    return findedday.date;
+                  }
+                });
+                if (inrange) {
+                  this.todayweather = todayw;
+                }
+              };
+              this.text = '';
+              this.time = '';
+              // if (!place) {
+              //   this.place = 'in';
+              // }else {
+              //   this.place = place;
+              // }
+              this.confirm = false;
+              this.hours = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+              this.BookAPIService = BookAPIService;
+              this.checkWeather();
+              this.close = () => $uibModalInstance.dismiss();
+            },
+          });
+        };
       },
     };
   });
